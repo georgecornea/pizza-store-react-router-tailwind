993e29d2df065e8fd4d3481da4faebaf62a459f9
George Cornea <george.cornea@outlook.com>
2024-05-04 10:34:21 -0700

    updated src/features/menu/Menu.jsx -> created and exported loader() function to get the data from the api, and got the data using useLoaderData() hook provided by react-router-dom. Updated src/App.jsx -> imported function loader() from Menu.jsx, and added as loaderMenu for the Menu route

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,19 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant.js";
+
 function Menu() {
-  return <h1>Menu</h1>;
+	const data = useLoaderData();
+	console.log(data);
+	return <h1>Menu</h1>;
+}
+
+// fetch the data from an api and return it
+// then export the function and import it in the file
+// where the createBrowserRouter() is, and added as a loader
+// to the element that needs the data
+export async function loader() {
+	const menu = await getMenu();
+	return menu;
 }
 
 export default Menu;
