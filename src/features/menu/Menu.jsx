ddae0b1d958ce1d521d6cfa2bc5f85750ec4b588
George Cornea <george.cornea@outlook.com>
2024-05-04 10:43:57 -0700

    Updated src/features/menu/Menu.jsx -> added <MenuItem/>

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,10 +1,21 @@
 import { useLoaderData } from "react-router-dom";
 import { getMenu } from "../../services/apiRestaurant.js";
+import MenuItem from "./MenuItem";
 
 function Menu() {
 	const data = useLoaderData();
-	console.log(data);
-	return <h1>Menu</h1>;
+	// console.log(data);
+
+	return (
+		<ul>
+			{data.map((pizza) => (
+				<MenuItem
+					pizza={pizza}
+					key={pizza.id}
+				/>
+			))}
+		</ul>
+	);
 }
 
 // fetch the data from an api and return it
