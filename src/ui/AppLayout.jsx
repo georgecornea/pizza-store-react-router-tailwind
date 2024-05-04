9974436b5f962f408ff313a18a79c2d0c97dd10a
George Cornea <george.cornea@outlook.com>
2024-05-04 10:52:05 -0700

    created src/ui/Loader.jsx -> a component to be displayed when loading data. Updated src/ui/AppLayout.jsx -> got loading data status using useNavigation() hook provided by react-router-doom. Used the loading status to conditionaly display the src/Loader.jsx when data is loading

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,18 @@
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
 
 export default function AppLayout() {
+	// hook provided by react-router-dom
+	// provides a state that can be loading, idle
+	const navigation = useNavigation();
+	const isLoading = navigation.state === "loading";
+
 	return (
-		<div>
+		<div className='layout'>
+			{isLoading && <Loader />}
+
 			<Header />
 			<main>
 				<Outlet />
