5cb3aedc89163cbf009765a9e38b77910c56c2ab
George Cornea <george.cornea@outlook.com>
2024-05-04 11:09:13 -0700

    Changed src/App.jsx -> updated createBrowserRouter() to display Error.jsx if any errors. Updated src/ui/Error.jsx to display the error message provided by useRouteError() hook.

--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -1,15 +1,18 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useRouteError } from "react-router-dom";
 
 function NotFound() {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
+	// hook provided by react-router-dom
+	// provides errors if any
+	const error = useRouteError();
 
-  return (
-    <div>
-      <h1>Something went wrong 😢</h1>
-      <p>%MESSAGE%</p>
-      <button onClick={() => navigate(-1)}>&larr; Go back</button>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Something went wrong 😢</h1>
+			<p>{error.data || error.message}</p>
+			<button onClick={() => navigate(-1)}>&larr; Go back</button>
+		</div>
+	);
 }
 
 export default NotFound;
