6b3c0aca4cecc4691b8196bb6b0b5e01abcb44e7
George Cornea <george.cornea@outlook.com>
2024-05-04 10:13:48 -0700

    Created components: src/ui/AppLayoutjsx, Header.jsx

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,14 @@
+import Header from "./Header";
+import CartOverview from "../features/cart/CartOverview";
+
+export default function AppLayout() {
+	return (
+		<div>
+			<Header />
+			<main>
+				<h1>Content</h1>
+			</main>
+			<CartOverview />
+		</div>
+	);
+}
