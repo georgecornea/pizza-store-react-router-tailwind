97b1b9ae34e24ab54bf6afe669f0f0cad611d0d7
George Cornea <george.cornea@outlook.com>
2024-05-04 18:49:33 -0700

    installed prettier-plugin-tailwindcss as per specifications from github

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,13 +1,13 @@
 function Home() {
-	return (
-		<div>
-			<h1 className='text-yellow-500 font-semibold'>
-				The best pizza.
-				<br />
-				Straight out of the oven, straight to you.
-			</h1>
-		</div>
-	);
+  return (
+    <div>
+      <h1 className=" font-semibold text-yellow-500">
+        The best pizza.
+        <br />
+        Straight out of the oven, straight to you.
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
