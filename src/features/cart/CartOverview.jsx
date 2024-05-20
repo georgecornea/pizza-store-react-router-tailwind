63e02d9802b456670e3d5d50df4404a074cf97d9
George Cornea <george.cornea@outlook.com>
2024-05-20 12:52:22 -0700

    Applied css styles to src/features/cart/CartOverview.jsx, erc/ui/Header.jsx and Home.jsx.

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 function CartOverview() {
   return (
-    <div className="bg-stone-800 uppercase text-stone-200">
-      <p className="font-semibold">
+    <div className="bg-stone-800 p-4 uppercase text-stone-200">
+      <p className="space-x-4 font-semibold">
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,8 +1,16 @@
+import CreateUser from "../features/user/CreateUser";
+
 function Home() {
   return (
-    <div className="text-center text-lg font-semibold text-yellow-400">
-      <h1 className="text-stone-800">The best pizza.</h1>
-      <p>Straight out of the oven, straight to you.</p>
+    <div className="my-10 text-center text-lg font-semibold text-yellow-400">
+      <h1 className="mb-8 text-stone-800">
+        The best pizza.
+        <br />
+        <span className="text-yellow-400">
+          Straight out of the oven, straight to you.
+        </span>
+      </h1>
+      <CreateUser />
     </div>
   );
 }
